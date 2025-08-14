2ae036886a3c81f8b0e11d5c2d44344ebe66d02b
Davipol <davi-liz@hotmail.it>
2025-08-14 20:23:28 +0100

    center input & button and improve responsiveness

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="w-full bg-cyan/20 h-40 flex shadow z-50 backdrop-blur-sm">
-      <h1 className="m-auto text-5xl font-bold">e-Tymology</h1>
+    <div className="w-full bg-cyan/20 h-40 flex shadow z-50 backdrop-blur-sm border-b-0">
+      <h1 className="m-auto text-5xl font-bold ">e-Tymology</h1>
     </div>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 export default function Home() {
   const [question, setQuestion] = useState("");
   const [answer, setAnswer] = useState("");
@@ -30,26 +31,30 @@ export default function Home() {
   return (
     <>
       <Header />
-      <div className="flex justify-center m-0 my-50 min-h-96">
-        <div className="bg-white flex flex-col">
-          <form onSubmit={handleSubmit}>
+
+      <div className="flex justify-center flex-col my-30 mx-5 lg:mx-40">
+        <div className="bg-white flex-col w-full ">
+          <form className="mx-auto  w-1/2 " id="word" onSubmit={handleSubmit}>
             <input
-              placeholder="Ask your question..."
+              className="w-full h-10 border-1 rounded"
+              id="word"
+              placeholder="Insert a word or a sentence..."
               value={question}
               onChange={(e) => setQuestion(e.target.value)}
             />
             <button
-              className="mt-2 bg-blue-600 text-white px-4 py-2 rounded"
+              className="w-full mt-1 mx-auto  bg-blue-600 text-white text-xl px-4 py-2 rounded"
               type="submit"
               disabled={loading || !question.trim()}
             >
               {loading ? "Thinking..." : "Ask"}
             </button>
           </form>
-
           {answer && (
-            <div className="mt-4">
-              <h2 className="font-bold text-lg">Here is your answer:</h2>
+            <div className="h-fit space-y-3">
+              <h2 className="font-bold text-lg text-center">
+                Here is your answer:
+              </h2>
               <p>
                 <strong>Modern meaning:</strong> {answer.modernMeaning || "N/A"}
               </p>
