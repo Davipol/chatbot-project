4238816c48edc6a691e9c324e973446caea7941f
Davipol <davi-liz@hotmail.it>
2025-09-05 15:03:57 +0100

    Add word 'Clear' below History Bar Clear button

--- src/app/components/HistoryBar.jsx
+++ src/app/components/HistoryBar.jsx
@@ -11,16 +11,22 @@ const HistoryBar = ({
 }) => {
   return (
     <div className="w-60 sm:w-60 md:w-64 lg:w-72 min-h-screen border-r-3 border-t-3 border-stone-300 rounded-tr-lg overflow-hidden inline-block bg-stone-100 dark:bg-stone-900 text-black  dark:text-white">
-      <div className="relative border-b-3 ">
-        <h2 className="font-bold text-2xl my-3 text-center pr-10 ">History</h2>
-        <GrClear
-          size={30}
-          title="Clear History"
-          className="absolute right-3 top-1/3 -translate-y-1/2 text-red-600 cursor-pointer hover:text-red-700 dark:text-red-400 dark:hover:text-red-200"
-          onClick={() => {
-            onClearHistory();
-          }}
-        />
+      <div className="relative border-b-3 pb-1 flex justify-center items-center">
+        <h2 className="font-bold text-2xl my-3 text-center pr-10">History</h2>
+
+        <div
+          className="absolute right-3 top-1/2 -translate-y-1/2 flex flex-col items-center cursor-pointer"
+          onClick={() => onClearHistory()}
+        >
+          <GrClear
+            size={25}
+            className="text-red-600 hover:text-red-700 dark:text-red-400 dark:hover:text-red-200"
+            title="Clear History"
+          />
+          <span className="text-red-600 dark:text-red-400 text-xs font-semibold hover:text-red-700">
+            Clear
+          </span>
+        </div>
       </div>
       <div className="max-h-[calc(100vh-80px)] overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400 hover:scrollbar-thumb-gray-500 scrollbar-track-transparent dark:scrollbar-thumb-zinc-600">
         <ul className="space-y-2">
